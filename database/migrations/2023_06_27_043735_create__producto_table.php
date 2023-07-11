2913fdbfbd97e665218d01b00765a6597f667943
jamescito <jr76407900@gmail.com>
2023-07-10 22:33:43 -0600

    Inicializacion del proyecto

---
+++ database/migrations/2023_06_27_043735_create__producto_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_producto', function (Blueprint $table) {
+            $table->id();
+            $table->string('codigoproducto',50)->unique();
+            $table->string('nombre_p',50);
+            $table->string('marca',50);
+            $table->string('precio_compra',50);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_producto');
+    }
+};
