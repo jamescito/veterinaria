895be83faf85626a040b62d711a972b75287013b
jamescito <jr76407900@gmail.com>
2023-07-10 22:47:37 -0600

    terminamos de configurar bootstrap 5 en laravel 9 mas vite

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Instalar Bootstrap 5 en Laravel 9 y Vite | Diario del Programador</title>
+    <title>Veterinaria</title>
 
 @vite(['resources/js/app.js', 'resources/css/app.scss'])
   </head>
