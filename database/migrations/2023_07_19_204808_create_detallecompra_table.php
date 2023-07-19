869a20432e44d685e48f972ed95cbc778454ba3f
jamescito <jr76407900@gmail.com>
2023-07-19 15:19:47 -0600

    Agregamos las tablas compra y detallecompra a la base de datos

---
+++ database/migrations/2023_07_19_204808_create_detallecompra_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detallecompra', function (Blueprint $table) {
+            $table->id();
+            $table->string('idcompra',50);
+            $table->string('codigoproducto',50);
+            $table->string('precio_compra',50);
+            $table->string('descripcion_producto',50);
+            $table->foreign('codigoproducto')->references('codigoproducto')->on('prodducto')->onDelete('cascade');
+            $table->foreign('idcompra')->references('idcompra')->on('compra')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detallecompra');
+    }
+};
